19afd4570e6ec2d34e1a4fd11956f4334d9aeded
daniel evertsson <daniel.evertsson@umu.se>
2015-10-23 08:54:14 +0200

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+__author__ = 'danielevertsson'
+
+from setuptools import setup
+
+setup(
+    name='VOPaaS test environment',
+    version='0.0.1',
+    description='',
+    author='DIRG',
+    author_email='dirg@its.umu.se',
+    license='Apache 2.0',
+)
